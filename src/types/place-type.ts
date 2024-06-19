c77108cb0754db94fcede4b937167117d5ae83cb
gen <kyliki@yandex.ru>
2024-06-20 00:22:47 +0300

    типизировал объекты в ответе с сервера

---
+++ src/types/place-type.ts
@@ -0,0 +1,11 @@
+export interface IPlace {
+  name: string;
+  place_id: string;
+  adm_area1: string;
+  adm_area2: any;
+  country: string;
+  lat: string;
+  lon: string;
+  timezone: string;
+  type: string;
+}

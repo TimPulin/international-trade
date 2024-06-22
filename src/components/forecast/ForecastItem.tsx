674ac96c0d476eee30845dd64990e027f1d4518f
gen <kyliki@yandex.ru>
2024-06-22 14:00:54 +0300

    стили для кнопки превью

--- src/components/forecast/ForecastItem.tsx
+++ src/components/forecast/ForecastItem.tsx
@@ -1,6 +1,7 @@
 import { ISituationOnDate } from '@/types/meteo-type';
 import itemStyle from './item.module.css';
 import iconStyle from './small-meteo-icons.module.css';
+import ButtonBase from '../button/ButtonBase';
 
 type ForecastItemPropsType = {
   item: ISituationOnDate;
@@ -15,13 +16,21 @@ export default function ForecastItem(props: ForecastItemPropsType) {
         ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
   };
 
+  const onClick = () => {
+    console.log('click');
+  };
+
   return (
-    <li key={item.date} className={itemStyle.item}>
-      <div
-        className={`${iconStyle.sprite} ${iconStyle.icon} ${iconStyle[`icon_${item.icon}`]}`}
-      ></div>
-      {item.temperature}°C
-      <div className={itemStyle.date}>{getDate()}</div>
+    <li key={item.date}>
+      <ButtonBase onClick={onClick} additionalClass={itemStyle.item}>
+        <>
+          <div
+            className={`${iconStyle.sprite} ${iconStyle.icon} ${iconStyle[`icon_${item.icon}`]}`}
+          ></div>
+          {item.temperature}°C
+          <div className={itemStyle.date}>{getDate()}</div>
+        </>
+      </ButtonBase>
     </li>
   );
 }
